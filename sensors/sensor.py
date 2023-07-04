4cdf53c0a7cc53a2e18fea0a060ed74ae4a025f6
Harshiv Saravanan <harshivs2002@gmail.com>
2023-07-04 22:26:02 +0530

    sensor.py

--- sensors/sensor.py
+++ sensors/sensor.py
@@ -1,21 +1,32 @@
+import random
 # Function to Read turbidity
 def read_turbidity():
-    try:
-        # Reading turbidity code here
-        turbidity = 100 # Generating Dummy values
-    except:
-        turbidity = None
+    result = random.choices([1,0], [0.1,0.9])[0]
+    if result == 0:
+        try:
+            noise = random.uniform(-3,3)
+            # Reading turbidity code here
+            turbidity = 100 + noise # Generating Dummy values
+        except:
+            turbidity = None
+    else:
+        turbidity = 1000 #Insert Anomalies
 
     return turbidity
 
 
 # Function to read temperature
 def read_temp():
-    try:
-        # Reading turbidity code here
-        temp = 100 # Generating Dummy values
-    except:
-        temp = None
+    result = random.choices([1,0], [0.01,0.99])[0]
+    if result == 0:
+        try:
+            noise = random.uniform(-3,3) 
+            # Reading turbidity code here
+            temp = 100 + noise # Generating Dummy values
+        except:
+            temp = None
+    else:
+        temp = 1000 #Insert Anomalies
 
     return temp
 
